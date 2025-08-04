418405ae444e1c327b47043c0b02c846b8014f9c
yajiyi <yajiyi@outlook.com>
2025-08-04 13:57:36 +0800

    Enhance functionality and UI by adding password management features, updating button actions, and creating message and password boxes.

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version. */
 
-const { app, BrowserWindow, globalShortcut } = require('electron');
-const { Worker, isMainThread } = require('worker_threads');
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const child_process = require('child_process');
+const { title } = require('process');
 
 app.disableHardwareAcceleration();
 
-let win;
-const createWindow = () => {
+let win, messageBoxWin, passwordBoxWin, messageBoxTitle, messageBoxMessage;
+function createWindow() {
     win = new BrowserWindow({
         width: 300,
         height: 500,
@@ -22,7 +23,7 @@ const createWindow = () => {
     });
     win.setMenuBarVisibility(false);
     win.loadFile('index.html');
-    win.resizable = false;
+    win.setResizable(false);
 
     win.on('minimize', () => {
         win.setSkipTaskbar(true);
@@ -42,6 +43,66 @@ const createWindow = () => {
         clearInterval(topInterval);
     });
 };
+
+function messageBox(title, message) {
+    messageBoxTitle = title;
+    messageBoxMessage = message;
+    messageBoxWin = new BrowserWindow({
+        width: 300,
+        height: 200,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+    });
+    messageBoxWin.setMenuBarVisibility(false);
+    messageBoxWin.loadFile('messagebox.html');
+    messageBoxWin.setResizable(false);
+    messageBoxWin.setAlwaysOnTop(true);
+
+    messageBoxWin.on('close', () => {
+        messageBoxWin = null;
+    });
+}
+
+ipcMain.on('getMessage', (event) => {
+    event.sender.send('message', messageBoxTitle, messageBoxMessage);
+});
+
+function passwordBox() {
+    passwordBoxWin = new BrowserWindow({
+        width: 400,
+        height: 550,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+    });
+    passwordBoxWin.setMenuBarVisibility(false);
+    passwordBoxWin.loadFile('passwordbox.html');
+    passwordBoxWin.setResizable(false);
+    passwordBoxWin.setAlwaysOnTop(true);
+
+    passwordBoxWin.on('close', () => {
+        passwordBoxWin = null;
+    });
+}
+
+ipcMain.on('getPasswordData', (event) => {
+    const rawData = [
+        { '极域': 'bcsy(一室) qwer1234(二三室)' },
+        { '联想': 'qwer1234(一二三室)' },
+        { '体育馆/游泳馆': 'qwer1234' },
+        { '宿舍': '未知' },
+        { '地下车库': '115830' },
+        { '监控': 'Hik12345' }
+    ];
+    const passwordData = rawData.map(item => {
+        const key = Object.keys(item)[0];
+        return { key, value: item[key] };
+    });
+    event.sender.send('passwordData', passwordData);
+});
 
 app.whenReady().then(() => {
     createWindow();
@@ -70,3 +131,30 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
+
+ipcMain.on('operation', (event, operation) => {
+    if (operation === 'killMythware') {
+        child_process.exec('taskkill /f /im studentmain.exe', (stdout) => {
+            console.log(`${stdout}`);
+        });
+        messageBox('操作完成', '');
+    } else if (operation === 'killLenovo') {
+
+        messageBox('操作完成', '');
+    } else if (operation === 'removeRestrictions') {
+        child_process.exec('sc stop TDFileFilter', (stdout) => {
+            console.log(`${stdout}`);
+        });
+        child_process.exec('sc stop TDNetFilter', (stdout) => {
+            console.log(`${stdout}`);
+        });
+        messageBox('操作完成', '需要重启网络适配器或重新插拔网线才能解除上网限制');
+    } else if (operation === 'enableWinKey') {
+        child_process.exec('reg delete "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Keyboard Layout" /v Scancode Map /f', (stdout) => {
+            console.log(`${stdout}`);
+        });
+        messageBox('操作完成', '需要注销才能生效');
+    } else if (operation === 'getPassword') {
+        passwordBox();
+    }
+})
